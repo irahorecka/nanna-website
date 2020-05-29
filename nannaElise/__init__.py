8d1f79591c5314a597257eeb0896d1a5af35777a
Ira Horecka <ira89@icloud.com>
2020-05-28 21:23:27 -0700

    add user authentication login logout account

--- nannaElise/__init__.py
+++ nannaElise/__init__.py
@@ -1,9 +1,16 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['SECRET_KEY'] = 'a56a4d216cfdac7b852a76efd4fdc5e1'  # make this an environment variable at some point
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
 
 from nannaElise import routes
--- nannaElise/routes.py
+++ nannaElise/routes.py
@@ -1,25 +1,26 @@
-from flask import render_template, request, url_for, flash, redirect
-from nannaElise import app
+from flask import render_template, request, url_for, flash, redirect, request
+from nannaElise import app, db, bcrypt
 from nannaElise.forms import RegistrationForm, LoginForm
-from nannaElise.models import Post
+from nannaElise.models import User, Post
+from flask_login import login_user, logout_user, current_user, login_required
 
 posts = [
     {
-        'author': 'Nanna Elise',
-        'title': 'Blog Post 1',
-        'content': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus euismod ligula ante, eu convallis arcu ullamcorper id. Nulla facilisi. Morbi egestas metus vel ex ultricies interdum. Pellentesque ultricies, leo in eleifend pretium, ex augue eleifend magna, placerat sodales metus lectus ut turpis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nunc rutrum vulputate risus a dictum. Nulla venenatis pretium elit, quis volutpat lacus iaculis non. Pellentesque felis magna, elementum in semper vel, placerat ac lorem. In eleifend elit vel eleifend semper. Aliquam eget pellentesque sapien.',
+        'author': '奈菜',
+        'title': 'ビーガンて何？',
+        'content': '英語圏のみならず、アルファベットを使用している国々において、なんらかの業務やデザインでダミー文を表示しておくときに使われる伝統的な文章があります。Lorem ipsumと呼ばれているその文は、現在では使われなくなった古いラテン語の文献をもとに、現代の英語と文字の出現頻度を合わせて生み出された無意味な文章です。',
         'date_posted': 'April 20, 2018'
     },
     {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Mauris et nunc elit. Curabitur consequat ante quis finibus tempus. Vestibulum cursus urna sed enim tristique, sit amet facilisis risus dictum. Quisque ac ligula mattis, imperdiet dolor sed, condimentum sem. Duis eget urna non orci ultrices aliquam eget sit amet ante. Pellentesque maximus tincidunt scelerisque. Duis vehicula velit nisl, vel sodales est congue eget. In hendrerit lacus vel ex dictum, non dignissim mi scelerisque. Nam volutpat, magna non pellentesque fermentum, ex ipsum fringilla dui, quis ultricies neque mi pretium sem. Donec finibus a neque sit amet luctus. Aenean mattis, lorem sed cursus bibendum, turpis leo ultrices nulla, in egestas ante sem a velit. Quisque volutpat dui leo, vitae vulputate nibh tincidunt id.',
+        'author': '田村慎一',
+        'title': 'ビーガン大好き！',
+        'content': 'コンテンツの内容ではなく、純粋にデザインを評価する場合にこれらの文章は役にたちます。人間は文章を読んでその意味を理解してしまうと、その他の部分への評価に影響を与えてしまうと言われていることがその理由です。',
         'date_posted': 'April 21, 2018'
     },
     {
-        'author': 'Ira Horecka',
-        'title': 'Blog Post 3',
-        'content': 'Phasellus ut iaculis orci, sed dictum nibh. Vivamus accumsan libero sit amet mauris commodo volutpat. Nullam feugiat ligula eget ligula auctor feugiat. Vestibulum in nisi consectetur, facilisis odio vitae, mollis tortor. Proin facilisis felis at tortor dapibus convallis. Aliquam erat volutpat. Nam at gravida nisi. Nulla facilisi. Donec sed lacus purus. Aliquam lacinia ligula ut hendrerit facilisis.',
+        'author': '相良',
+        'title': '今日の気分',
+        'content': 'では、日本語の場合はどうでしょう。前述のラテン文字とは違い、日本語の表記体系は非常に複雑です。ひらがな・カタカナ・漢字・数字・アルファベットを織り交ぜて記述されるため、Lorem ipsumでは全く代用できません。',
         'date_posted': 'April 21, 2018'
     }
 ]
@@ -55,22 +56,46 @@ def blog():
     return render_template("blog.html", title=title, posts=posts)
 
 
+# FORMS
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+
     form = RegistrationForm()
     if form.validate_on_submit():
-        flash(f"Account created for {form.username.data}!", 'success')
-        return redirect(url_for('home'))
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash(f"Your has been created! You may now log in.", 'success')
+        return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():
-        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
-            flash("You have been logged in!", 'success')
-            return redirect(url_for('home'))
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
         else:
-            flash("Login Unsuccessful. Please check username and password", 'danger')
+            flash("Login unsuccessful. Please check email and password", 'danger')
     return render_template('login.html', title='Login', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+
+@app.route('/account')
+@login_required
+def account():
+    return render_template('account.html', title='Account')
