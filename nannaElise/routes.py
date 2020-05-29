031c25c22a0c28d0160c59d2a28b91beb4b3d1b1
Ira Horecka <ira89@icloud.com>
2020-05-29 10:46:10 -0700

    update account form and template - account update and image update available

--- nannaElise/routes.py
+++ nannaElise/routes.py
@@ -1,6 +1,9 @@
+import os
+import secrets
+from PIL import Image
 from flask import render_template, request, url_for, flash, redirect, request
 from nannaElise import app, db, bcrypt
-from nannaElise.forms import RegistrationForm, LoginForm
+from nannaElise.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from nannaElise.models import User, Post
 from flask_login import login_user, logout_user, current_user, login_required
 
@@ -68,7 +71,7 @@ def register():
         user = User(username=form.username.data, email=form.email.data, password=hashed_password)
         db.session.add(user)
         db.session.commit()
-        flash(f"Your has been created! You may now log in.", 'success')
+        flash("Your has been created! You may now log in.", 'success')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
 
@@ -95,7 +98,38 @@ def logout():
     return redirect(url_for('home'))
 
 
-@app.route('/account')
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)  # underscore represents an unused variable
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/profile_img', picture_fn)
+
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    if i.mode in ("RGBA", "P"):
+        i = i.convert("RGB")
+    i.save(picture_path)
+
+    return picture_fn
+
+
+@app.route('/account', methods=['GET', 'POST'])
 @login_required
 def account():
-    return render_template('account.html', title='Account')
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Your account has been updated!', 'success')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+
+    image_file = url_for('static', filename='profile_img/' + current_user.image_file)
+    return render_template('account.html', title='Account', image_file=image_file, form=form)
