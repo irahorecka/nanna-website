642b358febafa74e99a5aeb7b5d822cb24121a6e
Ira Horecka <ira89@icloud.com>
2020-06-01 17:37:04 -0700

    add pagination and user posts

--- nannaElise/routes.py
+++ nannaElise/routes.py
@@ -35,7 +35,8 @@ def recipes():
 @app.route("/blog")
 def blog():
     title = "Blog"
-    posts = Post.query.all()
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
     return render_template("blog.html", title=title, posts=posts)
 
 
@@ -163,3 +164,12 @@ def delete_post(post_id):
     db.session.commit()
     flash('Your post has been deleted!', 'success')
     return redirect(url_for('home'))
+
+
+@app.route("/user/<string:username>")
+def user_posts(username):
+    page = request.args.get('page', 1, type=int)
+    user = User.query.filter_by(username=username).first_or_404()
+    title = user.username
+    posts = Post.query.filter_by(author=user).order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
+    return render_template("user_posts.html", title=title, posts=posts, user=user)
