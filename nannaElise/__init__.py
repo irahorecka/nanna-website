ce96f8ead8a436a7e6b4637b64d5fe81cd4ef98b
Ira Horecka <ira89@icloud.com>
2020-06-06 13:07:01 -0700

    update error pages - final tutorial with corey schafer

--- nannaElise/__init__.py
+++ nannaElise/__init__.py
@@ -25,8 +25,10 @@ def create_app(config_class=Config):
     from nannaElise.users.routes import users
     from nannaElise.posts.routes import posts
     from nannaElise.main.routes import main
+    from nannaElise.errors.handlers import errors
     app.register_blueprint(users)
     app.register_blueprint(posts)
     app.register_blueprint(main)
+    app.register_blueprint(errors)
 
     return app
